1f047cb55ae28e5b71163aa3bfe9f184298534ba
Valentin Mitran <ToolsVM@pm.me>
2020-04-17 01:13:20 +0100

    Front: Auth Pages

--- web/src/pages/AuthPage/LoginPage.jsx
+++ web/src/pages/AuthPage/LoginPage.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { TextField, Button } from "@material-ui/core";
+import "./AuthPage.scss";
 
 const LoginPage = () => {
-  return <>LoginPage</>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="auth-page">
+        <div className="auth-wrapper">
+          <form onSubmit={handleLogin}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              variant="outlined"
+              style={{ "margin-bottom": "10px" }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="outlined"
+              style={{ "margin-bottom": "10px" }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              style={{ "margin-bottom": "10px" }}
+            >
+              Login
+            </Button>
+          </form>
+          <Link to="/register">Register</Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
--- web/src/pages/AuthPage/RegisterPage.jsx
+++ web/src/pages/AuthPage/RegisterPage.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { TextField, Button } from "@material-ui/core";
+import "./AuthPage.scss";
 
 const RegisterPage = () => {
-  return <>RegisterPage</>;
-};
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="auth-page">
+        <div className="auth-wrapper">
+          <form onSubmit={handleRegister}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              variant="outlined"
+              style={{ "margin-bottom": "10px" }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="outlined"
+              style={{ "margin-bottom": "10px" }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              style={{ "margin-bottom": "10px" }}
+            >
+              Register
+            </Button>
+          </form>
+          <Link to="/login">Login</Link>
+        </div>
+      </div>
+    </>
+  );
+};
 export default RegisterPage;
