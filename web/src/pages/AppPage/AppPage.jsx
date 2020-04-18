9380e21fa1f1690ed13064a55b19691f74f72338
Valentin Mitran <ToolsVM@pm.me>
2020-04-18 04:09:04 +0100

    App Page: Navigation

--- web/src/pages/AppPage/AppPage.jsx
+++ web/src/pages/AppPage/AppPage.jsx
@@ -1,22 +1,59 @@
-import React from "react";
-import { MdNotifications } from "react-icons/md";
+import React, { useEffect, useState } from "react";
+import {
+  MdNotifications,
+  MdMenu,
+  MdHome,
+  MdPeople,
+  MdStore,
+  MdApps,
+} from "react-icons/md";
+import { Link } from "react-router-dom";
 import "./AppPage.scss";
 
 const AppPage = () => {
+  const [isSideOpen, setIsSideOpen] = useState(false);
   return (
     <>
       <div className="app">
         <div className="topbar">
+          <MdMenu id="menuToggler" onClick={() => setIsSideOpen(!isSideOpen)} />
           <div className="user-area">
             <div className="balance">&#36;16</div>
             <MdNotifications />
             <img
-              src="https://lh3.googleusercontent.com/-yiwpbXgTmYE/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJPMcgnrP1cptGUy-IH3cMbPHhfVIg/s88-c-k-c0x00ffffff-no-rj-mo/photo.jpg"
+              src="https://avatars2.githubusercontent.com/u/40249132?s=460&u=67b6f37dabe04c908c42671e961f3e50076e2d63&v=4"
               alt="Profile"
             />
           </div>
         </div>
-        <div className="sidebar"></div>
+        <div className={isSideOpen ? "sidebar-full" : "sidebar-shrinked"}>
+          <img
+            src="https://powerssquared.com/wp-content/uploads/2017/01/Reaper_500.png"
+            alt="Logo"
+          />
+          <Link>
+            <MdHome />
+            <span>Dashboard</span>
+          </Link>
+          <Link>
+            <MdPeople />
+            <span>Social</span>
+          </Link>
+          <Link>
+            <MdStore />
+            <span>Store</span>
+          </Link>
+          <Link>
+            <MdApps />
+            <span>Apps</span>
+          </Link>
+        </div>
+        {isSideOpen ? (
+          <div
+            className="ghost-wrap"
+            onClick={() => setIsSideOpen(false)}
+          ></div>
+        ) : null}
       </div>
     </>
   );
